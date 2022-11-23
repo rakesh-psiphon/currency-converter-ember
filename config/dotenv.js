2634bd817286f84fa1c97ee8123ac9a85d8a8974
rakesh-psiphon <77203910+rakesh-psiphon@users.noreply.github.com>
2022-11-23 21:44:36 +0530

    Update dotenv.js

--- config/dotenv.js
+++ config/dotenv.js
@@ -1,6 +1,6 @@
 module.exports = function () {
   return {
-    clientAllowedKeys: ['GEOAPIFY_API_KEY'],
+    clientAllowedKeys: ['GEOAPIFY_API_KEY','ROOT_URL'],
     // Fail build when there is missing any of clientAllowedKeys environment variables.
     // By default false.
     failOnMissingKey: false,
